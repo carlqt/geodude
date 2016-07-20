395b49bb73d387e5566fa92d29e0847ea2354d0f
Carl Tablante <carl.q.tablante@gmail.com>
2016-07-21 06:54:38 +0800

    Building with iris

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"github.com/carlqt/geodude/geocode"
-	"os"
+	// "github.com/carlqt/geodude/geocode"
+	"github.com/kataras/iris"
+	"github.com/iris-contrib/middleware/logger"
 )
 
 func checkErr(err error) {
@@ -11,12 +11,36 @@ func checkErr(err error) {
 		panic(err)
 	}
 }
+
+type User struct {
+	Name string
+	Age int
+}
+
 func main() {
-	apiKey := os.Getenv("GOOGLE_SERVER_KEY")
-	url := "https://maps.googleapis.com/maps/api/geocode/json"
+	// apiKey := os.Getenv("GOOGLE_SERVER_KEY")
+	// url := "https://maps.googleapis.com/maps/api/geocode/json"
 
-	g := geocode.GoogleGeoCode{URL: url, ApiKey: apiKey}
-	lng, lat := g.Geocode("xxlkajflkasdjfx")
+	// g := geocode.GoogleGeoCode{URL: url, ApiKey: apiKey}
+	// lng, lat := g.Geocode("xxlkajflkasdjfx")
 
-	fmt.Printf("Latitude is %f and Longitude is %f", lat, lng)
+	// fmt.Printf("Latitude is %f and Longitude is %f", lat, lng)
+
+	iris.Use(logger.New(iris.Logger))
+	iris.Post("/search", Search)
+
+	errorLogger := logger.New(iris.Logger)
+
+
+    iris.OnError(iris.StatusNotFound, func(ctx *iris.Context) {
+        errorLogger.Serve(ctx)
+        ctx.Write("My Custom 404 error page ")
+    })
+
+	iris.Listen(":8080")
+}
+
+func Search(c *iris.Context) {
+	u := User{Name: "Madeline", Age: 16}
+	c.JSON(iris.StatusOK, u)
 }
