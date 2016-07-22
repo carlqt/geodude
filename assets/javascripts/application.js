e2ad47d2b61bc75588e1dc8d640997149a8ad2df
Carl Tablante <carlwilliam.tablante@gmail.com>
2016-07-22 17:12:56 +0800

    Added table and mock data to display

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -2,6 +2,18 @@
 var newApp = angular.module('newApp', []);
 
 newApp.config(function($interpolateProvider){
-  $interpolateProvider.startSymbol('<<');
-  $interpolateProvider.endSymbol('>>');
+  $interpolateProvider.startSymbol('<%');
+  $interpolateProvider.endSymbol('%>');
+});
+
+newApp.controller('newAppController', function newAppController($scope) {
+  $scope.locations = [{
+    address: "Bugis",
+    latitude: 1.234,
+    longitude: 23.344
+  },
+  { address: "Ubi Avenue 1",
+    latitude: 24.4223,
+    longitude: 11.22123 
+  }];
 });
