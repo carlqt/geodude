0337f843e7e0667a774868a4c6b39f846d764ef8
Carl Tablante <carl.q.tablante@gmail.com>
2016-07-27 08:30:26 +0800

    Set create field to blank on enter

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -20,12 +20,13 @@ newApp.controller('newAppController', function newAppController($scope, $http) {
     $scope.locations = data;
   });
 
-  $scope.searchNearby = function($event){
+  $scope.addAddress = function($event){
     var keyCode = $event.which || $event.keyCode;
     if (keyCode === 13) {
 
       $http.post('/api/property', "location=" + $event.currentTarget.value, config).success(function(data, status, header){
         $scope.locations.push(data)
+        $scope.createField = ""
         console.log("created");
       }).error(function(data, status, header){
         console.log("Failed");
