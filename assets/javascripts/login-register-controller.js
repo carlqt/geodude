3b59d1cfbea8b58619fa3e47feb5b51d378a9888
Carl Tablante <carl.q.tablante@gmail.com>
2016-08-06 07:13:45 +0800

    configured ng-show for session forms

--- assets/javascripts/login-register-controller.js
+++ assets/javascripts/login-register-controller.js
@@ -1,5 +1,25 @@
+newApp.directive('diva', function() {
+    var directive = {};
+
+    directive.restrict = 'A'; /* restrict this directive to elements */
+
+    directive.template = "My first directive: {{textToInsert}}";
+
+    return directive;
+});
+
+
 newApp.controller('loginRegisterController', function newAppController($scope, $http, $httpParamSerializer) {
+  $scope.registerForm = false;
+  $scope.loginForm = true;
+
   $scope.showRegister = function() {
-    console.log("Yes");
+    $scope.registerForm = true;
+    $scope.loginForm = false;
+  };
+
+  $scope.showLogin = function() {
+    $scope.registerForm = false;
+    $scope.loginForm = true;
   };
 });
