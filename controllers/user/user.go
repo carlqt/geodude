d2b8c55840a54fdad835d187b6844be46fd254fe
Carl Tablante <carl.q.tablante@gmail.com>
2016-08-27 09:13:35 +0800

    Moved controller resource to separate packages

---
+++ controllers/user/user.go
@@ -0,0 +1,11 @@
+package controllers
+
+import(
+  "github.com/gin-gonic/gin"
+  "github.com/fatih/color"
+  "github.com/carlqt/geodude/geocode"
+  "net/http"
+  "strconv"
+  "github.com/carlqt/geodude/models"
+)
+
